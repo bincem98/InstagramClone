9a5cdfe57d52a681a0a6a1fe8f1913d2e81b977a
bincem98 <vmadamba20@gmail.com>
2021-02-28 17:46:47 -0800

    Upload

---
+++ app/src/main/java/com/example/instagramclone/ParseApplication.java
@@ -0,0 +1,25 @@
+package com.example.instagramclone;
+
+import android.app.Application;
+
+import com.parse.Parse;
+import com.parse.ParseObject;
+
+class ParseApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        // Register your parse models
+        ParseObject.registerSubclass(Post.class);
+
+
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                .applicationId("LiRW9UK7tv7VmG5elReMxw0ts1zC38ScYRXzFb82")
+                .clientKey("LVAIIOAEKUNDOSTgq4Hc2vFikevDxfVfKHlriDoM")
+                .server("https://parseapi.back4app.com")
+                .build()
+        );
+    }
+}
